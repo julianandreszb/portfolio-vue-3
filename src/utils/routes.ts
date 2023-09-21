d5ed7922c3b00efbf1d2f57f2613448651d694fe
jzapata <jzapata@lifefile.net>
2023-09-20 22:20:40 -0500

    implement route interfaces and routes map

---
+++ src/utils/routes.ts
@@ -0,0 +1,12 @@
+import type { IRoute } from '@/utils/tsinterfaces/IRoute'
+
+const routes: Array<IRoute> = [
+  { id: 'nav-item-about', label: 'About Me', path: '/about' },
+  { id: 'nav-item-skills', label: 'Skills', path: '/skills' },
+  { id: 'nav-item-portfolio', label: 'Portfolio', path: '/portfolio' },
+  { id: 'nav-item-career', label: 'Career', path: '/career' },
+  { id: 'nav-item-contact', label: 'Contact', path: '/contact' },
+  { id: 'nav-item-contact', label: 'Contact', path: '/contact' }
+]
+
+export { routes }
