173a8ac480f0f2c64a288d51c4232b7459e89562
jzapata <julianandreszb@gmail.com>
2023-10-26 01:01:12 -0500

    create base project container

--- src/components/blocks/VPortfolio.vue
+++ src/components/blocks/VPortfolio.vue
@@ -3,6 +3,7 @@ import VTagFilter from '@/components/VTagFilter.vue'
 import appConfig from '../../../app.config.json'
 import { reactive } from 'vue'
 import type { ITag, TagFilterOperation } from '@/utils/typeScriptDefinitions/ITag'
+import VCard from '@/components/VCard.vue'
 const { portfolio } = appConfig.sections
 
 const filteredTags = reactive(portfolio.tags)
@@ -27,7 +28,63 @@ function handleTagSelected(tagOperation: TagFilterOperation) {
 
 <template>
   <VTagFilter :tags="filteredTags" @emitTagSelected="handleTagSelected" />
-  {{ filteredTags }}
+  <section class="project-container">
+    <VCard
+      :card="{
+        title: 'Test',
+        description: 'description description description',
+        tags: [
+          {
+            id: 'javascript',
+            label: 'JavaScript',
+            isActive: false
+          },
+          {
+            id: 'html',
+            label: 'HTML',
+            isActive: false
+          }
+        ]
+      }"
+    />
+  </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@import '../../assets/sass/abstracts/mixin';
+
+.project-container {
+  display: flex;
+  //align-items: flex-start;
+  //align-content: flex-start;
+  gap: 1.6rem;
+  //flex-shrink: 0;
+  //align-self: stretch;
+  flex-wrap: wrap;
+  border: 2px solid rgba(230, 230, 230, 0.6);
+  background-color: var(--color-background-soft);
+  padding-block: 1.6rem;
+  padding-inline: 1.6rem;
+
+  :deep(.tag-container) {
+    span {
+      display: inline-block;
+      padding-block: 0.1rem;
+      padding-inline: 0.8rem;
+      border-radius: 16px;
+      background-color: #e6e6e6;
+
+      color: var(--color-text-black-60);
+      font-size: 1.6rem;
+      font-weight: 400;
+      //user-select: none;
+    }
+
+    @include for-size(tablet-landscape-up) {
+      span {
+        //font-size: 2.4rem;
+      }
+    }
+  }
+}
+</style>
