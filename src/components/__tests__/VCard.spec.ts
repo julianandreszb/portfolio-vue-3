54d50a68c6ea3d1fdd81934d7776b5d8e5f7377e
jzapata <julianandreszb@gmail.com>
2023-10-31 21:22:45 -0500

    VCard.vue unit tests

--- src/components/__tests__/VCard.spec.ts
+++ src/components/__tests__/VCard.spec.ts
@@ -4,41 +4,78 @@ import { mount } from '@vue/test-utils'
 import VCard from '@/components/VCard.vue'
 
 describe('VCard', () => {
-  let wrapper
-
-  beforeAll(async () => {
-    wrapper = mount(VCard, {
-      props: {
-        card: {
-          title: 'Binary To Decimal',
-          description: 'Description description description description description',
-          tags: [
-            {
-              id: 'typescript',
-              label: 'Typescript',
-              isActive: false
-            },
-            {
-              id: 'vue',
-              label: 'Vue 3.x',
-              isActive: false
-            },
-            {
-              id: 'mongo_db',
-              label: 'MongoDB',
-              isActive: false
-            }
-          ]
+  const props = {
+    card: {
+      isActive: true,
+      id: 'custom-id-1',
+      title: 'Binary To Decimal',
+      description: 'Description description description description description',
+      linkIcons: [
+        {
+          id: 'git',
+          label: '',
+          style: {
+            'background-color': 'transparent'
+          },
+          icon: {
+            class: 'bi-github'
+          },
+          isActive: false
+        },
+        {
+          id: 'demo',
+          label: '',
+          style: {
+            'background-color': 'transparent'
+          },
+          icon: {
+            class: 'bi-box-arrow-up-right'
+          },
+          isActive: false
         }
-      }
-    })
+      ],
+      tags: [
+        {
+          id: 'typescript',
+          label: 'Typescript',
+          isActive: false
+        },
+        {
+          id: 'vue',
+          label: 'Vue 3.x',
+          isActive: false
+        },
+        {
+          id: 'mongo_db',
+          label: 'MongoDB',
+          isActive: false
+        }
+      ]
+    }
+  }
+
+  const wrapper = mount(VCard, {
+    props
   })
+  beforeAll(async () => {})
 
   it('checks that title renders properly', () => {
-    // expect(wrapper.html()).contain('<span>Binary To Decimal</span>')
-    console.log(wrapper.html())
+    expect(wrapper.find('h3').text()).toBe(props.card.title)
   })
-  it('checks that description renders properly', () => {})
-  it('checks that tags render properly', () => {})
-  it('checks that project link icons render properly', () => {})
+  it('checks that description renders properly', () => {
+    expect(wrapper.find('p').text()).toBe(props.card.description)
+  })
+  it('checks that tags are rendered properly', () => {
+    expect(wrapper.find('[data-testid="typescript"]').text()).toBe('Typescript')
+    expect(wrapper.find('[data-testid="vue"]').text()).toBe('Vue 3.x')
+    expect(wrapper.find('[data-testid="mongo_db"]').text()).toBe('MongoDB')
+  })
+  it('checks that project link icons render properly', () => {
+    expect(wrapper.find('.link-icon-container > span[data-testid="git"]').html()).contain(
+      'class="bi-github"'
+    )
+    expect(wrapper.find('.link-icon-container > span[data-testid="demo"]').html()).contain(
+      'class="bi-box-arrow-up-right"'
+    )
+  })
 })
