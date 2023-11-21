2d7367ff0f24ad479f4699bf0790d667ebe36ac7
jzapata <julianandreszb@gmail.com>
2023-11-20 21:34:18 -0500

    fix html based on validator.w3.org

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  // history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHashHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   scrollBehavior(to) {
     if (to.hash) {
       return {
